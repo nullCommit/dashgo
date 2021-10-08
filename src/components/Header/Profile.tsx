696b0c59d59af0eb2482ed173e7c2d582d806550
nullCommit <allangeek@gmail.com>
2021-10-08 14:06:36 -0300

    Separa componentes do Header, Pagination e SideBar

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
+
+export function Profile() {
+  return (
+    <Flex align='center'>
+      <Box mr='4' textAlign='right'>
+        <Text>Allan Duarte</Text>
+        <Text color='gray.300' fontSize='small'>
+          allangeek@gmail.com
+        </Text>
+      </Box>
+
+      <Avatar
+        size='md'
+        name='Allan Duarte'
+        src='https://github.com/nullCommit.png'
+      />
+    </Flex>
+  );
+}

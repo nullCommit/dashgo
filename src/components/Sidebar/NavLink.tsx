6b78f1847c36c747479a6fa6edef123c26cb6e6a
allangeek <allangeek@gmail.com>
2021-12-07 16:45:27 -0300

    Implementa o fluxo de navegaçao

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,23 +1,27 @@
 import { ElementType } from 'react';
 import {
-  Link,
+  Link as ChakraLink,
   Icon,
   Text,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' align='center' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium' color=''>
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display='flex' align='center' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium' color=''>
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   HStack,
   Button,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
@@ -20,7 +21,7 @@ export default function CreateUser() {
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['6', '8']}>
           <Heading size='lg' fontWeight='normal'>
             Criar usuário
           </Heading>
@@ -28,12 +29,12 @@ export default function CreateUser() {
           <Divider my='6' borderColor='gray.700' />
 
           <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
               <Input name='name' label='Nome completo'></Input>
               <Input name='email' type='email' label='E-mail'></Input>
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['6', '8']} w='100%'>
               <Input name='password' type='password' label='Senha'></Input>
               <Input
                 name='password_confirmation'
@@ -45,8 +46,12 @@ export default function CreateUser() {
 
           <Flex mt='8' justify='flex-end'>
             <HStack spacing='4'>
-              <Button colorScheme='whiteAlpha'>Cancelar</Button>
-              <Button colorScheme='pink'>Salvar</Button>
+              <Link href='/users' passHref>
+                <Button colorScheme='whiteAlpha'>Cancelar</Button>
+              </Link>
+              <Button as='a' colorScheme='pink'>
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
